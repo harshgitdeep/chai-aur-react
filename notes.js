ce541b95e6cb709d4c0d552dfcf7cee92bc4d3d8
harshgitdeep <harshzone3@gmail.com>
2024-02-06 01:41:39 +0530

    commit

--- notes.js
+++ notes.js
@@ -1,23 +1,31 @@
 /*
 REACT NOTES:
 
+React latest documentation ---> https://react.dev/
+
 React library  React-DOM (for websites)
                React-native (for mobile applications)
 
+========================
 Ways to create react app - 
 
 npm (Node Package Manager) used for installing and managing Node.js packages, 
 npx (Node Package Executer) is a tool used to execute Node.js packages directly, without the need for installation.
 
+==
+
 METHOD 1: npx create-react-app appname (This method is very slow, very bulky)
 
+==
 
-METHOD 2: using vite 
+METHOD 2: using VITE  (vite -->https://vitejs.dev/)
 
+npm create vite@latest
 
+note: this method is fast, but there is no folder for node modules
+      use,   npm i --> to get node modules folder
 
-
-
+========================
 
 
 
---
+++ react-app/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React | Harshdeep Singh</h1>    
+  );
+}
+
+export default App;
