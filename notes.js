e3bb984eab26c11ce4e64d6585d44f431fe1f21b
harshgitdeep <harshzone3@gmail.com>
2024-02-06 01:16:40 +0530

    Started learning react wiht Chai-aur-react

---
+++ notes.js
@@ -0,0 +1,38 @@
+/*
+REACT NOTES:
+
+React library  React-DOM (for websites)
+               React-native (for mobile applications)
+
+Ways to create react app - 
+
+npm (Node Package Manager) used for installing and managing Node.js packages, 
+npx (Node Package Executer) is a tool used to execute Node.js packages directly, without the need for installation.
+
+METHOD 1: npx create-react-app appname (This method is very slow, very bulky)
+
+
+METHOD 2: using vite 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
